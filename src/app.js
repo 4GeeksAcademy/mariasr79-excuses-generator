db68b9b4f1151511495c72b3987e7373a3b216d2
mariasr79 <147938132+mariasr79@users.noreply.github.com>
2024-03-22 15:52:30 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function() {};
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+const obternerElementoAleatorio = listado => {
+  let elemento = "";
+  elemento = listado[Math.floor(Math.random() * listado.length)];
+  return elemento;
 };
+const alCargarLaPagina = () => {
+  let excusa = document.querySelector("#excuse");
+  excusa.innerHTML =
+    obternerElementoAleatorio(who) +
+    " " +
+    obternerElementoAleatorio(action) +
+    " " +
+    obternerElementoAleatorio(what) +
+    " " +
+    obternerElementoAleatorio(when);
+};
+window.onload = alCargarLaPagina();
